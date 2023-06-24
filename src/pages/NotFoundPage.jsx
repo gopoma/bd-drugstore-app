34f33658497a97a9ddd4de8a8d26ad33bd55a91e
Diego Pacori <dpacoria@unsa.edu.pe>
2023-06-24 00:45:04 -0500

    Se agrego estilos a la pagina de not found

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,9 +1,17 @@
+import classes from './NotFoundPage.module.css';
+
 export const NotFoundPage = () => {
   return (
-    <>
-      <h1>Not Found</h1>
-      <p>The resource that you&apos;re looking for is not available at the moment</p>
-    </>
+    <div className={classes['container']}>
+      
+      <div className={classes['container-not-found']}>
+        <img className={classes['img']} src="https://media.giphy.com/media/C21GGDOpKT6Z4VuXyn/giphy.gif" alt="not-found" />
+        <div className={classes['information']}>
+          <p className={classes['text']}>The resource that you&apos;re looking for is not available at the moment</p>
+        </div>
+      </div>
+      
+    </div>
   );
 };
 
