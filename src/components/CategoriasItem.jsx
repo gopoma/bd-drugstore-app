63360352021d39e91d33cfce525833e3bd9d70c9
gopoma <gordono@unsa.edu.pe>
2023-06-27 01:38:04 -0500

    add hover effect to table rows and buttons

--- src/components/CategoriasItem.jsx
+++ src/components/CategoriasItem.jsx
@@ -1,16 +1,19 @@
+import { useRef } from 'react';
 import { useCategoriasStore } from '../hooks';
 import classes from './CategoriasTable.module.css';
 
 export const CategoriasItem = ({ categoria }) => {
+  const checkRef = useRef();
   const { selectedCategorias, toggleSelectCategoria } = useCategoriasStore();
 
   return (
-    <tr>
+    <tr onClick={() => checkRef.current.click()}>
       <td className={` ${classes['table-header-checkbox']}`}>
         <input
           type="checkbox"
           checked={selectedCategorias.includes(categoria.CatCod)}
           onChange={() => toggleSelectCategoria(categoria.CatCod)}
+          ref={checkRef}
       /></td>
       <td className={` ${classes['table-header-codigo']}`}>{categoria.CatCod}</td>
       <td className={` ${classes['table-header-description']}`}>{categoria.CatDes}</td>
